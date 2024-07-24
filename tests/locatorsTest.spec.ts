6f743c488fb526ef871b487dcc0388c00e60e6d3
ispitkovskyi <spitkovskyigor@gmail.com>
2024-07-24 13:49:17 +0300

    user facing locators in tests

--- tests/locatorsTest.spec.ts
+++ tests/locatorsTest.spec.ts
@@ -37,3 +37,17 @@ test('Locator syntax rules', async({page}) => {
     // by exact text match
     page.locator(':text-is("Using the Grid")')
 })
+
+test('User facing locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+
+    // await page.getByTestId('value of data-testid attribute of element').click()
+})
