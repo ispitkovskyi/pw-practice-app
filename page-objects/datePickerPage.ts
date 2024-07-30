6785ba207b10b4d7a916f75489d67bb8b05505be
ispitkovskyi <spitkovskyigor@gmail.com>
2024-07-30 14:39:59 +0300

    page object with date picker

---
+++ page-objects/datePickerPage.ts
@@ -0,0 +1,48 @@
+import {Page, expect} from '@playwright/test'
+
+export class DatePickerPage {
+
+    private readonly page: Page
+    
+    constructor(page){
+        this.page = page
+    }
+
+    async selectCommonDatePickerDateFromToday(numberOfDaysFromToday: number){
+        const calendarInpuField = this.page.getByPlaceholder('Form Picker')
+        await calendarInpuField.click()
+        const dateToAssert = await this.selectDateInTheCalendar(numberOfDaysFromToday)
+        await expect(calendarInpuField).toHaveValue(dateToAssert)
+    }
+
+    async selectCommonDatePickerWithRangeFromToday(startDateFromToday: number, endDateFromToda: number){
+        const calendarInpuField = this.page.getByPlaceholder('Range Picker')
+        await calendarInpuField.click()
+        const dateToAssertStart = await this.selectDateInTheCalendar(startDateFromToday)
+        const dateToAssertEnd = await this.selectDateInTheCalendar(startDateFromToday)
+        
+        const dateRangeToAssert = `${dateToAssertStart} - ${dateToAssertEnd}`
+        await expect(calendarInpuField).toHaveValue(dateRangeToAssert)
+    }
+
+    private async selectDateInTheCalendar(numberOfDaysFromToday: number){
+        let date = new Date()
+        date.setDate(date.getDate() + numberOfDaysFromToday) // set date relatively of today
+        const expectedDate = date.getDate().toString() //expected date
+        const expectedMonthShort = date.toLocaleString('En-US', {month: 'short'}) // month of the expected date (short form)
+        const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'}) // month of the expected date (long form)
+        const expectedYear = date.getFullYear()                                 // year of the expected date
+        const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+    
+        let calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
+        const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `
+    
+        while(!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+            await this.page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
+            calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
+        }
+        await this.page.locator(".day-cell.ng-star-inserted").getByText(expectedDate, {exact: true}).click()
+
+        return dateToAssert
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
 import {FormLayoutsPage} from '../page-objects/formLayoutsPage'
+import { DatePickerPage } from '../page-objects/datePickerPage'
 
 // Before test (this is a Hook)
 test.beforeEach(async({page}) => {
@@ -16,11 +17,15 @@ test('navigate to form page', async({page}) => {
     await navigateTo.tooltipPage()
 })
 
-test('submit Using the Grid form', async({page}) => {
+test('parameterized methods', async({page}) => {
     const navigateTo: NavigationPage = new NavigationPage(page)
     const formLayoutsPage: FormLayoutsPage = new FormLayoutsPage(page)
+    const datePickerPage: DatePickerPage = new DatePickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
     await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'john@test.com', true)
+    await navigateTo.datepickerPage()
+    await datePickerPage.selectCommonDatePickerDateFromToday(5)
+    await datePickerPage.selectCommonDatePickerWithRangeFromToday(5, 10)
 })
