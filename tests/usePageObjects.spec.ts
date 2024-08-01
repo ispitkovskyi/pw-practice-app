def64d9f11a7f865ca94500c69ed735dd8da0750
ispitkovskyi <spitkovskyigor@gmail.com>
2024-08-01 15:45:34 +0300

    cmd scripts and random data generator

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
 
 // Before test (this is a Hook)
 test.beforeEach(async({page}) => {
@@ -17,9 +18,12 @@ test('navigate to form page', async({page}) => {
 
 test('parameterized methods', async({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName({firstName: 'John'})
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'john@test.com', true)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true)
     await pm.navigateTo().datepickerPage()
     await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5)
     await pm.onDatePickerPage().selectCommonDatePickerWithRangeFromToday(5, 10)
