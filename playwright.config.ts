d7796c45257d03c116243af6df33a0ca16efe851
ispitkovskyi <spitkovskyigor@gmail.com>
2024-08-06 18:24:47 +0300

    different reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,16 @@ export default defineConfig<TestOptions>({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+/*   reporter: [
+    ['html'],
+    ['json', {outputFile: 'test-results/jsonReport.json'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}], // Lots of CI/CD systems can read JUnit reports and publish them
+    ['allure-playwright']
+  ], */
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
